4c2198c71178d7ef883df0d8de4919f43582a08f
tmessall <tmessall17@gmail.com>
2020-10-21 23:11:55 -0600

    Cleaned up code and added comments

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const render = require("./lib/htmlRenderer");
 
 let empArr = [];
 
+// Recursive function that keeps going until employees are done
 function askNewEmployee() {
+    // Basic questions applying to any generic employee
     inquirer.prompt([
         {
             type: "list",
@@ -36,6 +38,7 @@ function askNewEmployee() {
             name: "email"
         }
     ]).then(response2 => {
+        // Ask intern specific questions
         if (response2.role === "Intern") {
             inquirer.prompt([
                 {
@@ -49,23 +52,27 @@ function askNewEmployee() {
                     name: "more"
                 }
             ]).then(response3 => {
+                // Make and add the intern
                 const newEmployee = new Intern(response2.name, response2.id, response2.email, response3.school);
                 empArr.push(newEmployee);
+                // Add more employees if needed
                 if (response3.more) {
                     askNewEmployee();
                 } else {
+                    // Create the html file
                     console.log("No more employees");
                     render(empArr);
-                    fs.writeFile("index.html", render(empArr), err => {
+                    fs.writeFile("team.html", render(empArr), err => {
                         if (err) {
                             console.log("There was an error writing the file.");
                         } else {
-                            console.log("File written as index.html.");
+                            console.log("File written as team.html.");
                         }
                     });
                 }
             })
         } else {
+            // Engineer specific questions
             inquirer.prompt([
                 {
                     type: "input",
@@ -78,17 +85,20 @@ function askNewEmployee() {
                     name: "more"
                 }
             ]).then(response3 => {
+                // Make and add the engineer
                 const newEmployee = new Engineer(response2.name, response2.id, response2.email, response3.school);
                 empArr.push(newEmployee);
+                // Add more employees if needed
                 if (response3.more) {
                     askNewEmployee();
                 } else {
+                    // Create the html file
                     console.log("No more employees");
-                    fs.writeFile("index.html", render(empArr), err => {
+                    fs.writeFile("team.html", render(empArr), err => {
                         if (err) {
                             console.log("There was an error writing the file.");
                         } else {
-                            console.log("File written as index.html.");
+                            console.log("File written as team.html.");
                         }
                     });
                 }
@@ -97,8 +107,7 @@ function askNewEmployee() {
     });
 }
 
-
-var newEmps = true;
+// Starts with manager questions
 inquirer.prompt([
     {
         type: "input",
@@ -122,6 +131,7 @@ inquirer.prompt([
     }
 ]).then(response1 => {
     try {
+        // Make and add the manager to empArr
         const newMgr = new Manager(response1.name, response1.id, response1.email, response1.officeNum);
         empArr = [newMgr];
         askNewEmployee();
@@ -129,29 +139,3 @@ inquirer.prompt([
         console.log("There was an error.");
     }
 });
-
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
